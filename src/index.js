9d8e29f92c7292a8c38be9cf10996d62fc2e2510
alya98 <alya-1998@list.ru>
2018-02-26 00:18:42 +0300

    done with tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function warmup(temperature) {
   // your implementation here
-    return (!isNaN(parseFloat(n)) && isFinite(temperature)) ? temperature*1.8+32: "вы ввели строку";
+    return temperature*1.8+32;
 };
